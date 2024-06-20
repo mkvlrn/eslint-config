683ee7afbd7bdcf1260e51cec3e23afd4800da48
Mike Valeriano <mkvlrn@gmail.com>
2024-06-20 20:10:15 -0300

    refactor: adjust config setup

--- configs.js
+++ configs.js
@@ -16,182 +16,146 @@ import pluginUnicorn from "eslint-plugin-unicorn";
 import pluginVitest from "eslint-plugin-vitest";
 import eslintTypescript, { parser } from "typescript-eslint";
 
-/** @type {import('typescript-eslint').ConfigWithExtends} */
-const eslintRecommended = {
-  name: "eslint recommended",
-  ...eslint.configs.recommended,
-};
-
-/** @type {import('typescript-eslint').ConfigWithExtends} */
-const consoleWarning = {
-  // reminds you to remove scattered console statements
-  name: "console warn",
-  rules: {
-    "no-console": "warn",
+/** @type {import('typescript-eslint').ConfigWithExtends[]} */
+const configs = [
+  {
+    name: "eslint recommended",
+    ...eslint.configs.recommended,
   },
-};
-
-/** @type {import('typescript-eslint').ConfigWithExtends} */
-const noDefaultExports = {
-  // absolutely* no default exports, please. code like a gentleman
-  name: "no default exports",
-  rules: {
-    "no-restricted-syntax": [
-      "error",
-      {
-        selector: "ExportDefaultDeclaration",
-        message: "Prefer named exports",
-      },
-    ],
+  {
+    // reminds you to remove scattered console statements
+    name: "console warn",
+    rules: {
+      "no-console": "warn",
+    },
   },
-};
-
-/** @type {import('typescript-eslint').ConfigWithExtends} */
-const noDefaultExportsExceptions = {
-  /**
-   * allows default exports for nextjs's page components (app router)
-   * some config files also need to export default, so yeah
-   */
-  name: "no default exports exceptions",
-  files: [
-    "src/app/**/{page,layout,template}.tsx",
-    "*.config.{ts,js}",
-    "**/*.config.{ts,js}",
-    "*.d.ts",
-    "**/*.d.ts",
-  ],
-  rules: {
-    "no-restricted-syntax": ["off", { selector: "ExportDefaultDeclaration" }],
+  {
+    // absolutely* no default exports, please. code like a gentleman
+    name: "no default exports",
+    rules: {
+      "no-restricted-syntax": [
+        "error",
+        {
+          selector: "ExportDefaultDeclaration",
+          message: "Prefer named exports",
+        },
+      ],
+    },
   },
-};
-
-/** @type {import('typescript-eslint').ConfigWithExtends} */
-const eslintPluginImport = {
-  name: "eslint plugin import",
-  plugins: { import: pluginImport },
-  settings: { "import/resolver": { typescript: {} } },
-  rules: {
-    // TODO - revisit these rules
+  {
     /**
-     * currently only used for the path aliases
-     * plugin doesn't support new eslint flat config
-     * and the maintainer is being a complete DICK about it
+     * allows default exports for nextjs's page components (app router)
+     * some config files also need to export default, so yeah
      */
-    // ...pluginImport.configs.recommended.rules,
-    // ...pluginImport.configs.typescript.rules,
-    // ...pluginImport.configs.react.rules,
-    // allows for the use of devDependencies in test files
-    // "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
-    // doesn't REQUIRE default exports, which is a silly idea to begin with
-    // "import/prefer-default-export": "off",
-  },
-};
-
-/** @type {import('typescript-eslint').ConfigWithExtends} */
-const eslintPluginUnicorn = {
-  name: "eslint plugin unicorn",
-  plugins: { unicorn: pluginUnicorn },
-  rules: {
-    ...pluginUnicorn.configs["flat/recommended"].rules,
-    // null is fine
-    "unicorn/no-null": "off",
-    // some names come from external sources, gotta adapt
-    "unicorn/prevent-abbreviations": [
-      "error",
-      {
-        allowList: { ProcessEnv: true, ImportMetaEnv: true },
-        ignore: ["next-env", "vite-env"],
-      },
+    name: "no default exports exceptions",
+    files: [
+      "src/app/**/{page,layout,template}.tsx",
+      "*.config.{ts,js}",
+      "**/*.config.{ts,js}",
+      "*.d.ts",
+      "**/*.d.ts",
     ],
+    rules: {
+      "no-restricted-syntax": ["off", { selector: "ExportDefaultDeclaration" }],
+    },
   },
-};
-
-/** @type {import('typescript-eslint').ConfigWithExtends} */
-const eslintPluginReactAndReactHooks = {
-  name: "eslint plugin react and react hooks",
-  files: ["*.tsx"],
-  plugins: { react: pluginReact, "react-hooks": pluginReactHooks },
-  rules: {
-    ...pluginReact.configs["jsx-runtime"].rules,
-    ...pluginReactHooks.configs.recommended.rules,
+  {
+    name: "eslint plugin import",
+    plugins: { import: pluginImport },
+    settings: { "import/resolver": { typescript: {} } },
+    rules: {
+      // TODO - revisit these rules
+      /**
+       * currently only used for the path aliases
+       * plugin doesn't support new eslint flat config
+       * and the maintainer is being a complete DICK about it
+       */
+      // ...pluginImport.configs.recommended.rules,
+      // ...pluginImport.configs.typescript.rules,
+      // ...pluginImport.configs.react.rules,
+      // allows for the use of devDependencies in test files
+      // "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
+      // doesn't REQUIRE default exports, which is a silly idea to begin with
+      // "import/prefer-default-export": "off",
+    },
   },
-};
-
-/** @type {import('typescript-eslint').ConfigWithExtends} */
-const eslintPluginNext = {
-  name: "eslint plugin next",
-  files: ["*.tsx"],
-  plugins: { "@next/next": pluginNext },
-  rules: {
-    ...pluginNext.configs["core-web-vitals"].rules,
+  {
+    name: "eslint plugin unicorn",
+    plugins: { unicorn: pluginUnicorn },
+    rules: {
+      ...pluginUnicorn.configs["flat/recommended"].rules,
+      // null is fine
+      "unicorn/no-null": "off",
+      // some names come from external sources, gotta adapt
+      "unicorn/prevent-abbreviations": [
+        "error",
+        {
+          allowList: { ProcessEnv: true, ImportMetaEnv: true },
+          ignore: ["next-env", "vite-env"],
+        },
+      ],
+    },
   },
-};
-
-/** @type {import('typescript-eslint').ConfigWithExtends} */
-const eslintPluginVitest = {
-  name: "eslint plugin vitest",
-  files: ["*.test.ts", "*.spec.ts", "*.test.tsx", "*.spec.tsx"],
-  plugins: { vitest: pluginVitest },
-  rules: {
-    ...pluginVitest.configs.recommended.rules,
+  {
+    name: "eslint plugin react and react hooks",
+    files: ["*.tsx"],
+    plugins: { react: pluginReact, "react-hooks": pluginReactHooks },
+    rules: {
+      ...pluginReact.configs["jsx-runtime"].rules,
+      ...pluginReactHooks.configs.recommended.rules,
+    },
   },
-};
-
-/** @type {import('typescript-eslint').ConfigWithExtends} */
-const disablePrettier = {
-  name: "disable prettier",
-  ...configPrettier,
-};
-
-/** @type {import('typescript-eslint').ConfigWithExtends} */
-const disableUnsafeTemporarily = {
-  /**
-   * temporarily disable while most npm packages
-   * still export unsafe types all over the place
-   * so I'm guessing until 2034 or something
-   */
-  name: "disable unsafe temporarily",
-  rules: {
-    "@typescript-eslint/no-unsafe-argument": "off",
-    "@typescript-eslint/no-unsafe-assignment": "off",
-    "@typescript-eslint/no-unsafe-call": "off",
-    "@typescript-eslint/no-unsafe-member-access": "off",
-    "@typescript-eslint/no-unsafe-return": "off",
+  {
+    name: "eslint plugin vitest",
+    files: ["*.test.ts", "*.spec.ts", "*.test.tsx", "*.spec.tsx"],
+    plugins: { vitest: pluginVitest },
+    rules: {
+      ...pluginVitest.configs.recommended.rules,
+    },
   },
-};
-
-/** @type {import('typescript-eslint').ConfigWithExtends} */
-const typescriptParser = {
-  name: "typescript parser",
-  languageOptions: {
-    parserOptions: { parser, ecmaVersion: "latest", project: true },
+  {
+    name: "disable prettier",
+    ...configPrettier,
   },
-};
+  {
+    /**
+     * temporarily disable while most npm packages
+     * still export unsafe types all over the place
+     * so I'm guessing until 2034 or something
+     */
+    name: "disable unsafe temporarily",
+    rules: {
+      "@typescript-eslint/no-unsafe-argument": "off",
+      "@typescript-eslint/no-unsafe-assignment": "off",
+      "@typescript-eslint/no-unsafe-call": "off",
+      "@typescript-eslint/no-unsafe-member-access": "off",
+      "@typescript-eslint/no-unsafe-return": "off",
+    },
+  },
+  {
+    name: "typescript parser",
+    languageOptions: {
+      parserOptions: { parser, ecmaVersion: "latest", project: true },
+    },
+  },
+];
 
 /**
  * @param {("base"|"next")} name
  * @returns {import('typescript-eslint').ConfigWithExtends[]}
  */
 export function createConfig(name) {
-  /** @type {import('typescript-eslint').ConfigWithExtends[]} */
-  const config = [
-    eslintRecommended,
-    ...eslintTypescript.configs.strictTypeChecked,
-    ...eslintTypescript.configs.stylisticTypeChecked,
-    consoleWarning,
-    noDefaultExports,
-    noDefaultExportsExceptions,
-    eslintPluginImport,
-    eslintPluginUnicorn,
-    eslintPluginReactAndReactHooks,
-    eslintPluginVitest,
-    disablePrettier,
-    disableUnsafeTemporarily,
-    typescriptParser,
-  ];
+  const config = [...configs];
 
   if (name === "next") {
-    config.push(eslintPluginNext);
+    config.push({
+      name: "eslint plugin next",
+      files: ["*.tsx"],
+      plugins: { "@next/next": pluginNext },
+      rules: {
+        ...pluginNext.configs["core-web-vitals"].rules,
+      },
+    });
   }
 
   const patchedConfig = config.map((config) => ({
